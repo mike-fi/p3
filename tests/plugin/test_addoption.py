9484bab705599704f93e4b47ee5fa902b9ae74d7
mk.fshr <mike.robin.fischer@web.de>
2025-07-28 22:00:39 +0200

    :alemic: :construction: WIP commit

--- tests/plugin/test_addoption.py
+++ tests/plugin/test_addoption.py
@@ -1,44 +1,52 @@
+from pytest import Pytester
+
 COLLECTION_FILE = """
-        import pytest
-        from pyspark.conf import SparkConf
-        from pyspark.sql import SparkSession
+import pytest
 
-        @pytest.mark.spark
-        def test_spark_config_fixture(spark):
-            assert isinstance(spark, SparkSession)
+@pytest.mark.spark
+def test_spark_marker():
+    assert True
 
-        def test_marker_addition(spark):
-            assert True
-    """
+def test_marker_deselection():
+    assert True
+"""
 
 CONFTEST_FILE = """
-        import pytest
-        pytest_plugins = "p3"
-    """
+import pytest
+pytest_plugins = ["p3"]
+"""
 
 
-def test_active_plugin(pytester):
+def test_active_plugin(pytester: Pytester):
     # create a temporary conftest.py file
+    pytester.makeconftest(CONFTEST_FILE)
+
+    # create a temporary pytest test file
+    pytester.makepyfile(COLLECTION_FILE)
     res = pytester.runpytest()
+    print(res)
     plugins_line = [s for s in res.outlines if s.startswith('plugins:')][0]
     assert 'pyspark-plugin' in plugins_line
 
 
-def test_available_fixtures(pytester):
+def test_available_fixtures(pytester: Pytester):
     # create a temporary conftest.py file
     pytester.makeconftest(CONFTEST_FILE)
     res = pytester.runpytest('--fixtures')
-    assert any('spark [session scope]' in line for line in res.outlines)
+    print(res.stdout.lines)
+    print(res.stderr.lines)
+    assert any('spark [session scope]' in line for line in res.stdout.lines)
 
 
-def test_oppressing_plugin(pytester):
+def test_oppressing_plugin(pytester: Pytester):
     # create a temporary conftest.py file
     pytester.makeconftest(CONFTEST_FILE)
     res = pytester.runpytest('-p no:p3')
+    print(res.outlines)
     assert not any('spark [session scope]' in line for line in res.outlines)
 
 
-def test_spark_marker(pytester):
+def test_spark_marker(pytester: Pytester):
     """Make sure that plugin works."""
     # create a temporary conftest.py file
     pytester.makeconftest(CONFTEST_FILE)
@@ -47,7 +55,7 @@ def test_spark_marker(pytester):
     pytester.makepyfile(COLLECTION_FILE)
 
     # run all tests with spark marker
-    result = pytester.runpytest('-m spark')
+    result = pytester.runpytest('--collect-only', '-m spark')
 
     # check that all 4 tests passed
     # We deselect the test that uses spark fixture tho, tbd.
@@ -55,15 +63,15 @@ def test_spark_marker(pytester):
 
 
 # @pytest.mark.xfail(reason='still select tests using SparkSession fixture.')
-def test_no_spark_marker(pytester):
+def test_no_spark_marker(pytester: Pytester):
     """Make sure that plugin works."""
     pytester.makeconftest(CONFTEST_FILE)
     pytester.makepyfile(COLLECTION_FILE)
 
     # run all tests without spark marker
     # Doesnt work at the moment due to string representation
-    result = pytester.runpytest('-m not spark')
+    result = pytester.runpytest('-m not spark', '--collect-only')
 
     # check that all 4 tests passed
     # Still problem with
-    result.assert_outcomes(passed=1, deselected=1)
+    result.assert_outcomes(skipped=1, deselected=1)
